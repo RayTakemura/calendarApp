d08a5a560eb934fec64004442d3ad44297afa372
Ray Takemura <raytakemura@gmail.com>
2021-03-11 23:49:45 -0800

    pseudo code for dynamic programming

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//global vars
+    // an array to store localStorage vars
+    // const of the time to start in hour
+    // const of the time to end in hour
+
+// Elements needed for each cell:
+// time, input (textarea), and save button
+
+// vars needed for each cell:
+    //moment object for each hour 
+
+// using above, create a function: populateSchedules
+    // for loop or "for each"
+
+// the 'on-click' function for saving tasks
+
+// the 'on-click' function for editting tasks
+
+// compareTime function to set color of each schedule
+    //check taskmaster-pro for details
+
+
+
+
+
+
+
+
+
+
+    
