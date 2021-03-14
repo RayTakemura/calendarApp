448da2859f6458d61700af012441699096fff197
Ray Takemura <raytakemura@gmail.com>
2021-03-14 15:29:05 -0700

    saving ability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 //global vars
-var tasks = {
-    id: [],
-    task: []
-};// an array to store localStorage vars
+var tasks = {};
 const START = 9; // const of the time to start in military hour
 const END = 17;// const of the time to end in military hour
 
@@ -10,46 +7,46 @@ const END = 17;// const of the time to end in military hour
  * Populates tasks on to the container.
  * Each task contains time in hour, schedule for the hour, and a save button
  */
-function populateTasks() {
-    // for loop or "for each"
-    for(var i = 0; i < (END - START + 1); i++){
-        // create row
-        let $row = $('<div>')
-            .addClass('row')
-            .attr('id-',START + i);
-        // create time for work hour
-        let time = moment().set({
-            'hour' : i + START,
-            'minute' : 0,
-            'second' : 0
-        });
-        //convert time into proper format and make a span element
-        let timeText = time.format('h A');
-        let $timePrint = $('<span>')
-            .addClass('col-1 border-top')
-            .text(timeText);
+function createTask(index, task) {
+    // create row
+    let $row = $('<div>')
+        .addClass('row')
+        .attr('id-',index);
+    // create time for work hour
+    let time = moment().set({
+        'hour' : index,
+        'minute' : 0,
+        'second' : 0
+    });
+    //convert time into proper format and make a span element
+    let timeText = time.format('h A');
+    let $timePrint = $('<span>')
+        .addClass('col-1 border-top')
+        .text(timeText);
 
-        //create p element that holds the text for schedule
-        let $taskP = $('<p>')
-            .addClass('col-10 border border-light m-0 pt-2')
-            .attr('id-p-', START + i);
-            //.text("You can do anything here. So don't worry about it. Be so very light. Be a gentle whisper. Maybe, just to play a little, we'll put a little tree here. Let's do it again then, what the heck. I want everbody to be happy. That's what it's all about.");
-
-        // create the save button with the lock icon
-        let $saveBtn = $('<button>')
-            .addClass('col-1 rounded-right saveBtn')
-            .attr('id-btn-', START + i); 
-        let $span = $('<span>')
-            .addClass('oi oi-lock-locked');
-        $saveBtn.append($span);
-
-        //append every column into the row
-        $row.append($timePrint, $taskP, $saveBtn);
-
-        //append row to container
-        $('.container').append($row);
+    //create p element that holds the text for schedule
+    var $taskP = $('<p>')
+        .addClass('col-10 border border-light m-0 pt-2')
+        .attr('id-p-', index);
+    if(task){
+        $taskP.text(task);
     }
-}
+
+    // create the save button with the lock icon
+    let $saveBtn = $('<button>')
+        .addClass('col-1 rounded-right saveBtn')
+        .attr('id-btn-', index); 
+    let $span = $('<span>')
+        .addClass('oi oi-lock-locked');
+    $saveBtn.append($span);
+
+    //append every column into the row
+    $row.append($timePrint, $taskP, $saveBtn);
+
+    //append row to container
+    $('.container').append($row);
+    console.log($row);
+};
 
 
 /**
@@ -69,20 +66,22 @@ $('.container').on('click', 'p', function() {
     $textInput.trigger('focus');
 });
 
+
 /**
  * Save the updated schedule to local storage.
  * Convert textarea element back to p.
  */
 $('.container').on('click', 'button', function() {
+//$('button').click( function() {    
     // check if the textarea exists
     let idNum = $(this).attr('id-btn-');
-    let $textArea = $('textarea').data('id-text-', idNum);
-    if($textArea) return;
-    // let $p = $('p').data('id-p-', idNum);
-    // if($p) return;
+    console.log(idNum);
+    let $textArea = $(this).prev();
+    console.log($textArea.attr('id-text-'));
     
     // obtain data attr, class, and text from textarea
     let pClass = $textArea.attr('class');
+    console.log($textArea.val())
     let text = $textArea
         .val()
         .trim();
@@ -92,53 +91,62 @@ $('.container').on('click', 'button', function() {
         .attr('id-p-', idNum)
         .addClass(pClass)
         .text(text);
-    $($textArea).replaceWith($taskP)
+    $($textArea).replaceWith($taskP);
 
     // save the task and the id
-    tasks.id.push(idNum);
-    tasks.task.push(text);
+    if(tasks.id.length > 0){
+        for (var i = 0; i < tasks.id.length; i++){
+            if(idNum === parseInt(tasks.id[i])){
+                tasks.task.splice(i, 1, text);
+                break;
+            } else if(i === tasks.id.length - 1 ){
+                tasks.id.push(idNum);
+                tasks.task.push(text);
+            } 
+        }
+    } else{
+        tasks.id.push(idNum);
+                tasks.task.push(text);
+    }
+    
     
     // store the saved task to the local storage
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
-})
+});
 
 // load the tasks
 function loadTasks(){
-
-    populateTasks();
-
     tasks = JSON.parse(localStorage.getItem('tasks'));
-
     if (!tasks) {
         tasks = {
-            id: [],
-            task: []
+          id: [],
+          task: []
         };
     }
-
-    console.log(tasks.id.length);
-
-    for (var i = 0; i < tasks.id.length; i++) {
-        $taskP = $('p').data('id-text-', tasks.id[i]);
-        //console.log($taskP.val());
-        console.log(tasks.task[i]);
-        $taskP.text(tasks.task[i]);
-        //$taskP.addClass('col-12');
+    console.log(tasks);
+    var j = 0;
+    for (var i = START; i < END + 1; i++) {
+        for (j = 0; j < tasks.id.length; j++){
+            console.log(parseInt(tasks.id[j]));
+            console.log(tasks.task[j]);
+            if(parseInt(tasks.id[j]) === i){
+                createTask(i,tasks.task[j]);
+                break; //breaks out of the inner for loop (the one with j variable)
+            }else if (j === tasks.id.length - 1){
+                createTask(i);
+                break; 
+            }
+        }
+        if (tasks.id.length === 0){
+            createTask(i);
+        }
     }
-
-}
+};
 
 
 // compareTime function to set color of each schedule
     //check taskmaster-pro for details
 
 
-
 loadTasks();
 
-
-
-
-
-    
